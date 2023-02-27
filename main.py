c574cfee4ec10cb07b3dd04721701e12babac112
Sam <samueltategoudie@gmail.com>
2023-02-28 10:12:18 +1100

    playing with ports

--- main.py
+++ main.py
@@ -42,5 +42,5 @@ def message():
 
 if __name__ == '__main__':
     # app.run(debug=True, port=os.getenv("PORT", default=5000))
-    app.run(host=os.getenv("HOST", default='0.0.0.0'), port=os.getenv("PORT"))
+    app.run(host=os.getenv("HOST", default='0.0.0.0'), port=os.getenv("PORT", default='3000'))
 
