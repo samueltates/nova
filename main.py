19100b3460f04860006f2ea292ec17a4eb61aaa7
Sam <samueltategoudie@gmail.com>
2023-05-25 11:59:44 +1000

    adding bacjk the bad transport value

--- main.py
+++ main.py
@@ -187,7 +187,7 @@ if __name__ == '__main__':
     port=int(os.getenv("PORT", default=5000))
     config = Config()
     config.bind = [str(host)+":"+str(port)]  # As an example configuration setting
-    # os.environ['AUTHLIB_INSECURE_TRANSPORT'] = '1'
+    os.environ['AUTHLIB_INSECURE_TRANSPORT'] = '1'
 
     asyncio.run(serve(app, config))
 
