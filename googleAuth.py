984ff48d2321bf8b72cc9362af23cd804308dd7e
Sam <samueltategoudie@gmail.com>
2023-05-25 12:35:46 +1000

    pushing all https te he

--- googleAuth.py
+++ googleAuth.py
@@ -42,7 +42,7 @@ def oauth2callback():
     state = userAuths['state']    
     flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
         'credentials.json', scopes=SCOPES, state=state)
-    flow.redirect_uri = url_for('oauth2callback', _external=True) 
+    flow.redirect_uri = url_for('oauth2callback', _external=True, _scheme='https')
 
     # Use the authorization server's response to fetch the OAuth 2.0 tokens.
     authorization_response = request.url
@@ -57,7 +57,7 @@ def oauth2callback():
     with open(userAuths['userID']+"-token.json", "w") as token:
         token.write(credentials.to_json())
     userAuths['authorised'] = True,
-    return redirect(url_for('authComplete', _external=True))
+    return redirect(url_for('authComplete', _external=True,  _scheme='https'))
     
 
 @app.route('/authComplete')
@@ -131,7 +131,7 @@ class GoogleDocsReader(BaseReader):
                 flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
                 'credentials.json',
                 scopes=['https://www.googleapis.com/auth/documents.readonly'])    
-                flow.redirect_uri = url_for('oauth2callback', _external=True)
+                flow.redirect_uri = url_for('oauth2callback', _external=True,  _scheme='https')
                 authorization_url, state = flow.authorization_url(
                     # Enable offline access so that you can refresh an access token without
                     # re-prompting the user for permission. Recommended for web server apps.
