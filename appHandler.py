264ecf9aa6ce71f90a966264462ec33ac834f278
Sam <samueltategoudie@gmail.com>
2023-05-30 23:01:01 +1000

    ccookie same site

--- appHandler.py
+++ appHandler.py
@@ -10,6 +10,7 @@ app.session = None
 app.config['SESSION_TYPE'] = 'redis'
 app.config["REDIS_URI"] = os.environ.get('REDIS_URI')
 app.config["REDIS_PORT"] = 6379
+app.config["SESSION_COOKIE_SAMESITE"] = None
 # app.config.update(SESSION_COOKIE_SAMESITE="None", SESSION_COOKIE_SECURE=True)
 app.config['SESSION_COOKIE_SECURE'] = os.environ.get('SESSION_COOKIE_SECURE')  # Set to True if using HTTPS!
 app.config["WEBSOCKET_MAX_SIZE"] = 1024 * 1024 * 100  # Maximum size set to 1MB (adjust as needed)
