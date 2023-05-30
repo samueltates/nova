c7203c805525d3dfc75ac52d7d5b21596a15fa8d
Sam <samueltategoudie@gmail.com>
2023-05-30 21:34:00 +1000

    adding secure cookie stuff

--- appHandler.py
+++ appHandler.py
@@ -11,6 +11,6 @@ app.config['SESSION_TYPE'] = 'redis'
 app.config["REDIS_URI"] = os.environ.get('REDIS_URI')
 app.config["REDIS_PORT"] = 6379
 # app.config.update(SESSION_COOKIE_SAMESITE="None", SESSION_COOKIE_SECURE=True)
-# app.config['SESSION_COOKIE_SECURE'] = True  # Set to True if using HTTPS!
+app.config['SESSION_COOKIE_SECURE'] = os.environ.get('SESSION_COOKIE_SECURE')  # Set to True if using HTTPS!
 app.config["WEBSOCKET_MAX_SIZE"] = 1024 * 1024 * 100  # Maximum size set to 1MB (adjust as needed)
 app.config["MAX_CONTENT_LENGTH"] = 1024 * 1024 * 100  # Setting the maximum request size to 100MB
