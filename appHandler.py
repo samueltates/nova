3362c3668624bdd3f42216b3fdc1d8fa0f87458d
Sam <samueltategoudie@gmail.com>
2023-05-31 10:32:03 +1000

    removed all from cors(app) (so in config instead.

--- appHandler.py
+++ appHandler.py
@@ -10,10 +10,12 @@ app.config['SESSION_TYPE'] = 'redis'
 # app.config["REDIS_URI"] = os.environ.get('REDIS_URI')
 # app.config["REDIS_PORT"] = 6379
 app.config['CORS_ALLOW_HEADERS'] = "Content-Type, Authorization"
+app.config['CORS_ALLOW_ORIGINS'] = os.environ.get("CORS_ALLOWED_ORIGINS")
 app.config['CORS_SUPPORTS_CREDENTIALS'] = True
 app.config['CORS_METHODS'] = "GET, POST, PUT, DELETE, OPTIONS"
+app.config['CORS_ALLOW_CREDENTIALS'] = True
 app.config["SESSION_COOKIE_SAMESITE"] = None
 app.config['SESSION_COOKIE_SECURE'] = os.environ.get('SESSION_COOKIE_SECURE')  # Set to True if using HTTPS!
 app.config["WEBSOCKET_MAX_SIZE"] = 1024 * 1024 * 100  # Maximum size set to 1MB (adjust as needed)
 app.config["MAX_CONTENT_LENGTH"] = 1024 * 1024 * 100  # Setting the maximum request size to 100MB
-app = cors(app, allow_origin=os.getenv("CORS_ALLOWED_ORIGINS"), allow_credentials = True)
+app = cors(app)
