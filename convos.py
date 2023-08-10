f430831c6fb42c333c123560fd6e204e7b986ec9
Sam <samueltategoudie@gmail.com>
2023-08-10 12:26:44 +1000

    loadout logsfix + bs4

--- convos.py
+++ convos.py
@@ -48,26 +48,28 @@ async def get_loadout_logs(sessionID):
         logs = await prisma.log.find_many(
             where={ "UserID": userID },
         )
-        if len(splitID) >=2:
-        #    
-            session ={
-                'id': log.id,
-                'sessionID' : log.SessionID,
-                'convoID' : splitID[1],
-                'date' : log.date,
-                'summary':log.summary,
-            }
-            available_convos[sessionID].append(session)
-        else:
-            session ={
-                'id': log.id,
-                'sessionID' : log.SessionID,
-                'convoID' : splitID,
-                'date' : log.date,
-                'summary': log.summary,
-            }
-            available_convos[sessionID].append(session)
-            # convos.append(splitID)
+        for log in logs:
+            splitID = log.SessionID.split('-')
+            if len(splitID) >=2:
+            #    
+                session ={
+                    'id': log.id,
+                    'sessionID' : log.SessionID,
+                    'convoID' : splitID[1],
+                    'date' : log.date,
+                    'summary':log.summary,
+                }
+                available_convos[sessionID].append(session)
+            else:
+                session ={
+                    'id': log.id,
+                    'sessionID' : log.SessionID,
+                    'convoID' : splitID,
+                    'date' : log.date,
+                    'summary': log.summary,
+                }
+                available_convos[sessionID].append(session)
+                # convos.append(splitID)
 
 
     await websocket.send(json.dumps({'event': 'populate_convos', 'payload': available_convos[sessionID]}))
