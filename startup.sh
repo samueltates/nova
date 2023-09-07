df9fd29e89af1179acb1eb533c0b3fbb3ef0b52b
samueltates <me@samueltate.com>
2023-09-08 09:16:29 +1000

    added config value

--- startup.sh
+++ startup.sh
@@ -2,5 +2,7 @@
 redis-cli flushall &
 redis-server &
 REDIS_PID=$!
+export FONTCONFIG_FILE=/etc/fonts/fonts.conf
+export XDG_CONFIG_HOME=$XDG_CONFIG_HOME/fontconfig
 convert -version
 pipenv run python main.py
