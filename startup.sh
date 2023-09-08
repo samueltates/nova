5c12bfe46d60cea10b992bdb8c0e214a858484ed
samueltates <me@samueltate.com>
2023-09-08 12:01:38 +1000

    dropping text line for now

--- startup.sh
+++ startup.sh
@@ -2,7 +2,17 @@
 redis-cli flushall &
 redis-server &
 REDIS_PID=$!
-export FONTCONFIG_FILE=/etc/fonts/fonts.conf
-export XDG_CONFIG_HOME=$XDG_CONFIG_HOME/fontconfig
 convert -version
 pipenv run python main.py
+
+RUN mkdir -p /tmp/distr && \
+    cd /tmp/distr && \
+    wget https://download.imagemagick.org/ImageMagick/download/releases/ImageMagick-7.0.11-2.tar.xz && \
+    tar xvf ImageMagick-7.0.11-2.tar.xz && \
+    cd ImageMagick-7.0.11-2 && \
+    ./configure --enable-shared=yes --disable-static --without-perl && \
+    make && \
+    make install && \
+    ldconfig /usr/local/lib && \
+    cd /tmp && \
+    rm -rf distr
