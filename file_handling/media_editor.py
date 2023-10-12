030c30a08514a308bfe1dd793f858ddcbdd25cf1
Sam <me@samueltate.com>
2023-10-12 21:05:19 +1100

    setting magic_font_path

--- file_handling/media_editor.py
+++ file_handling/media_editor.py
@@ -263,6 +263,7 @@ async def overlay_b_roll(main_video_cartridge, b_roll_to_overlay, sessionID, con
 
                     text_clip = TextClip(text.upper(), size = size, fontsize=font_size, color='white', kerning = kerning, method='caption', align='west', font = font_type, stroke_color='black', stroke_width=stroke_width, interline=interline)
                     eZprint(TextClip.search('Oswald', 'font'), ['OVERLAY', 'TRANSCRIBE'])
+                    eZprint(TextClip.list('font'), ['OVERLAY', 'TRANSCRIBE'])
                     text_clip = text_clip.set_duration(line_duration )
                     text_clip = text_clip.set_start(line_start )
                     # set position so its centered on x and 80% down on y
--- startup.sh
+++ startup.sh
@@ -8,8 +8,15 @@ echo $FONTCONFIG_PATH
 echo $FONTCONFIG_FILE
 export FONTCONFIG_PATH=/nix/store/$(ls -1 /nix/store | grep 'fontconfig-' | awk '!/-lib$|\.drv$|-bin$|\.tar.xz.drv$/')
 export FONTCONFIG_FILE="$FONTCONFIG_PATH/etc/fonts/fonts.conf"
+export IMAGE_MAGICK_PATH=/nix/store/$(ls -1 /nix/store | grep 'imagemagick-' | awk '!/-lib$|\.drv$|-bin$|\.tar.xz.drv$/')
+export IMAGE_MAGICK_POLICY_PATH="$IMAGE_MAGICK_PATH"/etc/ImageMagick-7/policy.xml
+export GOOGLE_FONT_PATH=/nix/store/$(ls -1 /nix/store | grep 'google-fonts-' | awk '!/-lib$|\.drv$|-bin$|\.tar.xz.drv$/')/etc/fonts
+export MAGIC_FONT_PATH="$GOOGLE_FONT_PATH"/share/fonts/truetype
 echo $FONTCONFIG_PATH
 echo $FONTCONFIG_FILE
+echo $GOOGLE_FONT_PATH
+echo $MAGIC_FONT_PATH
 echo $(fc-list)
+echo $(convert -list font)
 
 pipenv run python main.py
