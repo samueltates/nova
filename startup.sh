eb36bd71cab7d2923b5631da761132a721e53bdc
Sam <me@samueltate.com>
2023-10-14 09:07:49 +1100

    remove logs for startuos

--- startup.sh
+++ startup.sh
@@ -12,11 +12,11 @@ export IMAGE_MAGICK_PATH=/nix/store/$(ls -1 /nix/store | grep 'imagemagick-' | a
 export IMAGE_MAGICK_POLICY_PATH="$IMAGE_MAGICK_PATH"/etc/ImageMagick-7/policy.xml
 export GOOGLE_FONT_PATH=/nix/store/$(ls -1 /nix/store | grep 'google-fonts-' | awk '!/-lib$|\.drv$|-bin$|\.tar.xz.drv$/')/etc/fonts
 export MAGIC_FONT_PATH=/app/fonts/Oswald/static/Oswald-Bold.ttf
-echo $FONTCONFIG_PATH
-echo $FONTCONFIG_FILE
-echo $GOOGLE_FONT_PATH
-echo $MAGIC_FONT_PATH
-echo $(fc-list)
-echo $(convert -list font)
+# echo $FONTCONFIG_PATH
+# echo $FONTCONFIG_FILE
+# echo $GOOGLE_FONT_PATH
+# echo $MAGIC_FONT_PATH
+# echo $(fc-list)
+# echo $(convert -list font)
 
 pipenv run python main.py
