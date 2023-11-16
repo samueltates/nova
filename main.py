80ee360d48283e7f1a724969133fcd31efe39a5e
samueltates <me@samueltate>
2023-11-16 20:57:05 +1100

    start of interpreting voice transcript chunks

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ from tools.keywords import get_summary_from_keyword, get_summary_from_insight
 from core.loadout import add_loadout, get_loadouts, set_loadout, drop_loadout, set_read_only,set_loadout_title, update_loadout_field,clear_loadout, get_latest_loadout_convo
 from session.tokens import update_coin_count
 from file_handling.fileHandler import handle_file_start, handle_file_chunk, handle_file_end
+from file_handling.transcribe import handle_transcript_chunk, handle_transcript_end
 from version import __version__
 
 
@@ -648,6 +649,21 @@ async def process_message(parsed_data):
     if (parsed_data['type'] == 'get_models'):
         models = await getModels()
         await websocket.send(json.dumps({'event':'populate_models', 'payload': models}))
+    if (parsed_data['type'] == 'transcribe_chunk'):
+        convoID = parsed_data['data']['convoID']
+        recordingID = parsed_data['data']['recordingID']
+        chunkID = parsed_data['data']['chunkID']
+        chunk = parsed_data['data']['chunk']
+        # transcript_text = await handle_simple_transcript(chunk, chunkID)
+        transcript_text = await handle_transcript_chunk(convoID, recordingID, chunkID, chunk)
+        await websocket.send(json.dumps({'event':'return_chunk_transcript', 'convoID': convoID, 'recordingID': recordingID, 'chunkID' : chunkID, 'transcript_text': transcript_text}))
+
+    if (parsed_data['type'] == 'handle_transcript_end'):
+        convoID = parsed_data['data']['convoID']
+        recordingID = parsed_data['data']['recordingID']
+        updated_transcript = await handle_transcript_end(convoID, recordingID)
+        await websocket.send(json.dumps({'event':'transcript_end', 'convoID': convoID, 'recordingID': recordingID, 'updated_transcript' : updated_transcript}))
+
 
 
 file_chunks = {}
